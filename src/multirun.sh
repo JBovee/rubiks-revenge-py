10f1ff2d3bf83c9973f08980dcde43ef435781d4
jbovee <Mitouch.b@gmail.com>
2016-10-26 22:42:29 -0400

    Modified to be more concise. Many tests in one output file

--- src/multirun.sh
+++ src/multirun.sh
@@ -1,7 +1,8 @@
-CXPB="$1"
-MUTPB="$2"
-for GEN in 30 40 50 60 70 80 90 100
+MUTPB=0.01
+for GEN in 30 40 50 60; do for CXPB in 0.65 0.7 0.75 0.8;
 do
-    echo "$GEN"
-    python main.py $GEN $CXPB $MUTPB > sims/f1/f1_tourn_$CXPB.$MUTPB.$GEN
+    echo "fitness1, $GEN, $CXPB, $MUTPB"
+    python main.py $GEN $CXPB $MUTPB "f1" >> sims/f1/f1_test1
+    python main.py $GEN $CXPB $MUTPB "f1" >> sims/f1/f1_test2
+done
 done
