2eab40fb219f605f8c5f3e3f523cb8bc6b9a9c95
jbovee <Mitouch.b@gmail.com>
2016-11-10 12:04:14 -0500

    Small change so it doesn't always say 'fitness2'

--- src/multirun.sh
+++ src/multirun.sh
@@ -1,24 +1,33 @@
 MUTPB=0.01
 CXPB=0.8
-OUTPATH=simsV3
+FUNC="f7"
+OUTPATH=simsV3.2
 mkdir -p -- "$OUTPATH"
-OUTFILE=moves_div_20_under_20_moves
+OUTFILE=3_part_fitness_faces_centers_moves
 echo "$OUTFILE"
 for GEN in 40 50 60;
 do
-    echo "fitness2, $GEN, $CXPB, $MUTPB"
+    echo "$FUNC, $GEN, $CXPB, $MUTPB"
+    echo "*********************" >> $OUTPATH/$OUTFILE
     echo "fitness2, $GEN, $CXPB, $MUTPB" >> $OUTPATH/$OUTFILE
     echo "First"
     echo "First" >> $OUTPATH/$OUTFILE
-    python main.py $GEN $CXPB $MUTPB "f2" >> $OUTPATH/$OUTFILE
+    python main.py $GEN $CXPB $MUTPB $FUNC >> $OUTPATH/$OUTFILE
     echo "Second"
     echo "Second" >> $OUTPATH/$OUTFILE
-    python main.py $GEN $CXPB $MUTPB "f2" >> $OUTPATH/$OUTFILE
+    python main.py $GEN $CXPB $MUTPB $FUNC >> $OUTPATH/$OUTFILE
     echo "Third"
     echo "Third" >> $OUTPATH/$OUTFILE
-    python main.py $GEN $CXPB $MUTPB "f2" >> $OUTPATH/$OUTFILE
+    python main.py $GEN $CXPB $MUTPB $FUNC >> $OUTPATH/$OUTFILE
     echo "Fourth"
     echo "Fourth" >> $OUTPATH/$OUTFILE
-    python main.py $GEN $CXPB $MUTPB "f2" >> $OUTPATH/$OUTFILE
+    python main.py $GEN $CXPB $MUTPB $FUNC >> $OUTPATH/$OUTFILE
+    echo "Fifth"
+    echo "Fifth" >> $OUTPATH/$OUTFILE
+    python main.py $GEN $CXPB $MUTPB $FUNC >> $OUTPATH/$OUTFILE
+    echo "Sixth"
+    echo "Sixth" >> $OUTPATH/$OUTFILE
+    python main.py $GEN $CXPB $MUTPB $FUNC >> $OUTPATH/$OUTFILE
+    echo "*********************" >> $OUTPATH/$OUTFILE
     echo ""
 done
